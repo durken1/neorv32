bc6a54d50b0d8884ea1b531715caa6489d16ce1f
durken <git@durken.se>
2024-02-20 14:44:59 +0100

    add 7seg display functionality

--- sw/lib/include/de10_lite.h
+++ sw/lib/include/de10_lite.h
@@ -12,13 +12,67 @@
  *
  * @param[in] wdata Data word (32-bit) to store.
  **************************************************************************/
-inline void __attribute__ ((always_inline)) de10_lite_7seg_write(uint32_t wdata) {
+inline void __attribute__ ((always_inline)) de10_lite_7seg_write_unsigned_hex(uint32_t wdata) {
 
 //    neorv32_cpu_store_unsigned_word(90000000, wdata);
-  uint32_t reg_addr = 0x90000000;
+  uint32_t reg_addr_data = 0x90000000;
+  uint32_t reg_addr_setup = 0x90000004;
   uint32_t reg_data = wdata;
+  uint32_t reg_setup = 0x00000000;
 
-  asm volatile ("sw %[da], 0(%[ad])" : : [da] "r" (reg_data), [ad] "r" (reg_addr));
+  asm volatile ("sw %[da], 0(%[ad])" : : [da] "r" (reg_setup), [ad] "r" (reg_addr_setup));
+  asm volatile ("sw %[da], 0(%[ad])" : : [da] "r" (reg_data), [ad] "r" (reg_addr_data));
+}
+
+/**********************************************************************//**
+ * Output value to 7seg display
+ *
+ * @param[in] wdata Data word (32-bit) to store.
+ **************************************************************************/
+inline void __attribute__ ((always_inline)) de10_lite_7seg_write_signed_hex(int32_t wdata) {
+
+//    neorv32_cpu_store_unsigned_word(90000000, wdata);
+  uint32_t reg_addr_data = 0x90000000;
+  uint32_t reg_addr_setup = 0x90000004;
+  uint32_t reg_data = wdata;
+  uint32_t reg_setup = 0x00000001;
+
+  asm volatile ("sw %[da], 0(%[ad])" : : [da] "r" (reg_setup), [ad] "r" (reg_addr_setup));
+  asm volatile ("sw %[da], 0(%[ad])" : : [da] "r" (reg_data), [ad] "r" (reg_addr_data));
+}
+
+/**********************************************************************//**
+ * Output value to 7seg display
+ *
+ * @param[in] wdata Data word (32-bit) to store.
+ **************************************************************************/
+inline void __attribute__ ((always_inline)) de10_lite_7seg_write_unsigned_dec(int32_t wdata) {
+
+//    neorv32_cpu_store_unsigned_word(90000000, wdata);
+  uint32_t reg_addr_data = 0x90000000;
+  uint32_t reg_addr_setup = 0x90000004;
+  uint32_t reg_data = wdata;
+  uint32_t reg_setup = 0x00000002;
+
+  asm volatile ("sw %[da], 0(%[ad])" : : [da] "r" (reg_setup), [ad] "r" (reg_addr_setup));
+  asm volatile ("sw %[da], 0(%[ad])" : : [da] "r" (reg_data), [ad] "r" (reg_addr_data));
+}
+
+/**********************************************************************//**
+ * Output value to 7seg display
+ *
+ * @param[in] wdata Data word (32-bit) to store.
+ **************************************************************************/
+inline void __attribute__ ((always_inline)) de10_lite_7seg_write_signed_dec(int32_t wdata) {
+
+//    neorv32_cpu_store_unsigned_word(90000000, wdata);
+  uint32_t reg_addr_data = 0x90000000;
+  uint32_t reg_addr_setup = 0x90000004;
+  uint32_t reg_data = wdata;
+  uint32_t reg_setup = 0x00000003;
+
+  asm volatile ("sw %[da], 0(%[ad])" : : [da] "r" (reg_setup), [ad] "r" (reg_addr_setup));
+  asm volatile ("sw %[da], 0(%[ad])" : : [da] "r" (reg_data), [ad] "r" (reg_addr_data));
 }
 
 #endif // de10_lite_h
